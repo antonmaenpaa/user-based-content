4e36a642d1b3c46749bde8d664f5962e80493b6d
FIRST_GUSTAV LAST_ANDERSSON <Gustavkarlandersson@hotmail.com>
2021-04-20 13:43:57 +0200

    propsv2

--- client/src/components/masterview.js
+++ client/src/components/masterview.js
@@ -20,6 +20,16 @@ function MasterView() {
                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
                  nisi ut aliquip ex ea commodo consequat." 
                 />
+                <Post title="Lorem" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                 nisi ut aliquip ex ea commodo consequat." 
+                />
+                <Post title="Lorem" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                 nisi ut aliquip ex ea commodo consequat." 
+                />
 
             </div>
             
