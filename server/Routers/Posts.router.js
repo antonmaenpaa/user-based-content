69e8a4e295a5747eba058b586fd1bbc55943dc41
antonmaenpaa <antonmaenpaa@gmail.com>
2021-04-21 15:31:35 +0200

    add put endpont for posts

--- server/Routers/Posts.router.js
+++ server/Routers/Posts.router.js
@@ -8,19 +8,32 @@ const router = express.Router();
 router.get("/posts", async (req, res) => {
   const docs = await Posts.find({});
   res.status(200).json(docs);
+  // bind to componentDidMount
 });
 
 router.post("/posts", async (req, res) => {
   console.log(req.body);
   const doc = await Posts.create(req.body);
   res.status(200).json(doc);
+  // bind to create new post button and input fields
 });
 
 router.delete("/posts/:id", async (req, res) => {
   const id = req.params.id;
   const result = await Posts.findByIdAndDelete(id);
-
   res.send(result);
+  // bind to delete button
+});
+
+router.put("/posts/:id", async (req, res) => {
+  const id = req.params.id;
+  const updates = req.body;
+  const options = { new: true };
+
+  const result = await Posts.findByIdAndUpdate(id, updates, options);
+
+  res.status(200).json(result);
+  // bind to edit button with inputs
 });
 
 module.exports = router;
