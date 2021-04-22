3381006d271d6d97bfbf8d065aa14f059e74b956
antonmaenpaa <antonmaenpaa@gmail.com>
2021-04-22 12:00:00 +0200

    add endpoint for login register logout
    Co-authored-by: LejlaDolovac <LejlaDolovac@users.noreply.github.com>
    Co-authored-by: lilgujj <gustavkarlandersson@hotmail.com>
    Co-authored-by: jv98 <jv98@users.noreply.github.com>
    Co-authored-by: Victor Olsson <victor.olsson@hiveandfive.se>

--- server/DbModel/UserSchema.js
+++ server/DbModel/UserSchema.js
@@ -6,7 +6,6 @@ let Schema = mongoose.Schema;
 let UserSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  repeatpassword: { type: String, required: true },
 });
 
 let Users = mongoose.model("Users", UserSchema); // Our DB - collection name is Users
--- server/Routers/User.router.js
+++ server/Routers/User.router.js
@@ -1,23 +1,66 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const mongo = require("mongodb");
 const Users = require("../DbModel/UserSchema");
+const cookieSession = require('cookie-session');
+
+
 
 // Creates a user
 
+router.use(cookieSession({
+    name: "session",
+    secret: "cr23ackc0d3",
+    secure: false,
+    maxAge: 1000 * 10,
+    httpOnly: true
 
-router.post = async (req, res, err) => {
-  console.log("post");
-  try {
-    let userData = {
-      email: req.body.email,
-      password: req.body.password,
-    };
-    let responsData = await Users.create(userData);
-    res.status(200).send(responsData);
-  } catch (err) {
-    res.status(400).send(err.stack, "The data was not load");
+}));
+
+router.get('/users' ,async (req, res) => {
+
+  const users = await Users.find({})
+  res.status(200).json(users)
+});
+
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const user = await Users.create({
+    email: req.body.email,
+    password: hashedPassword,
+  }) 
+
+  res.status(201).json(user)
+  // bind to register input fields
+});
+
+router.post("/login", async (req, res) => {
+  
+  const { email, password } = req.body;
+  // console.log(email, password)
+ 
+  const users = await Users.find({});
+  const foundUser = await users.find(user => user.email === email)
+  console.log(foundUser)
+
+  if(!foundUser || !await bcrypt.compare(password, foundUser.password)) {
+    res.status(404).json("USER NOT FOUND")
+    return
   }
-};
+
+  req.session.username = foundUser._id
+
+  res.status(200).json(foundUser)
+  // bind to login btn to validate and proceed to posts and show post form
+})
+
+router.delete("/logout", (req,res) => {
+  req.session = null
+  res.status(200).json("LOGGED OUt")
+  // bind to logout btn
+});
+
 
 module.exports = router;
